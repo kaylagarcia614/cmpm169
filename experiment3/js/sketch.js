35029b5a384330d9d074afb74b9fe6124443212f
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-01-28 23:41:30 -0800

    added color

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -10,29 +10,32 @@ const myPI = (1 + Math.sqrt(5))/2
 var mouse;
 
 function setup() {
-	let canvas=createCanvas(600, 600);
-    canvas.parent('canvas-container');
-	background(0);
-	
-	rad = width;
-	mouse = createVector(mouseX, mouseY);
-	noStroke();
+  createCanvas(600, 600);
+  colorMode(HSB, 360, 100, 100);
+  
+  rad = width;
+  mouse = createVector(mouseX, mouseY);
+  noStroke();
 }
 
-function draw() {	
-	mytime = frameCount/100;
-	mouse.x = map(mouseX, 0, width, 0, 50);
-	mouse.y = map(mouseY, 0, width, 0, 20);
-	count = abs(cos(mytime/8)*1000);
-	
-	background(0);
-	push();
-	translate(width/2, height/2);
-	for(let i = 0; i <= count; i ++){
-		var x = cos(i * myPI * TWO_PI)*(i/count)*rad;
-		var y = sin(i * myPI * TWO_PI)*(i/count)*rad;
-		fill(255)
-		circle(x, y, abs(cos(i*mouse.y/count + mytime*2)*mouse.x));
-	}
-	pop();
+function draw() { 
+  mytime = frameCount/100;
+  mouse.x = map(mouseX, 0, width, 0, 50);
+  mouse.y = map(mouseY, 0, width, 0, 20);
+  count = abs(cos(mytime/8)*5000);
+  
+  background(0);
+  push();
+  translate(width/2, height/2);
+  for(let i = 0; i <= count; i ++){
+    var x = cos(i * myPI * TWO_PI)*(i/count)*rad;
+    var y = sin(i * myPI * TWO_PI)*(i/count)*rad;
+
+    // Calculate hue value based on the angle
+    var hueValue = map(atan2(y, x), -PI, PI, 0, 360);
+
+    fill(hueValue, 100, 100);
+    circle(x, y, abs(cos(i*mouse.y/count + mytime*2)*mouse.x));
+  }
+  pop();
 }
