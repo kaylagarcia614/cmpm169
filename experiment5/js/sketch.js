d5e3a7783e1f6676c4f5012a9bbff8ef7e0100f7
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-02-12 11:59:09 -0800

    changed color pallete to yellows and whites to make it look like stars

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -13,8 +13,6 @@ function draw() {
   background(0);
   noFill();
   strokeWeight(2);
-  stroke(0);
-
   scale(zoomFactor); // Apply zoom factor
 
   rotateX(angleX);
@@ -26,14 +24,14 @@ function draw() {
     let y = map(noise(noiseScale * i + 1000), 0, 1, -400, 400); 
     let z = map(noise(noiseScale * i + 2000), 0, 1, -400, 400); 
 
-    let r = map(sin(frameCount * 0.01 + x + y + z), -1, 1, 0, 255);
-    let g = map(sin(frameCount * 0.02 + x + y + z), -1, 1, 0, 255);
-    let b = map(sin(frameCount * 0.03 + x + y + z), -1, 1, 0, 255);
+    let r = map(sin(frameCount * 0.01 + x + y + z), -1, 1, 200, 255);
+    let g = map(sin(frameCount * 0.02 + x + y + z), -1, 1, 200, 255); 
+    let b = map(sin(frameCount * 0.03 + x + y + z), -1, 1, 200, 255); 
 
     push();
     translate(x, y, z);
     stroke(r, g, b);
-    ambientMaterial(r, g, b);
+    ambientMaterial(255); // White material for the spheres
     sphere(sphereSize);
     pop();
   }
@@ -45,6 +43,6 @@ function draw() {
 
 function mouseWheel(event) {
   zoomFactor += event.delta * 0.001; // Adjust zoom factor based on mouse scroll
-  zoomFactor = constrain(zoomFactor, 0.1, 10); 
+  zoomFactor = constrain(zoomFactor, 0.1, 10); // Constrain zoom factor to reasonable limits
   return false; // Prevent default browser behavior
 }
