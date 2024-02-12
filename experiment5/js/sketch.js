a93437505d9ffb33fcbb80175acac8315a3c1ae6
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-02-12 11:57:21 -0800

    instead of cube grid, randomly place based on perlin noise

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -2,7 +2,7 @@ let angleX = 0;
 let angleY = 0;
 let angleZ = 0;
 let sphereSize = 5; // Size of each sphere
-let gridSize = 5; // Number of spheres in each direction
+let noiseScale = 0.1; // Scale factor for Perlin noise
 
 function setup() {
   createCanvas(800, 800, WEBGL);
@@ -18,28 +18,24 @@ function draw() {
   rotateY(angleY);
   rotateZ(angleZ);
 
-  for (let i = -gridSize; i <= gridSize; i++) {
-    for (let j = -gridSize; j <= gridSize; j++) {
-      for (let k = -gridSize; k <= gridSize; k++) {
-        let r = map(sin(frameCount * 0.01 + i + j + k), -1, 1, 0, 255);
-        let g = map(sin(frameCount * 0.02 + i + j + k), -1, 1, 0, 255);
-        let b = map(sin(frameCount * 0.03 + i + j + k), -1, 1, 0, 255);
+  for (let i = 0; i < 1000; i++) { // Drawing 1000 spheres
+    let x = map(noise(noiseScale * i), 0, 1, -400, 400); // Generate x position based on Perlin noise
+    let y = map(noise(noiseScale * i + 1000), 0, 1, -400, 400); // Generate y position based on Perlin noise
+    let z = map(noise(noiseScale * i + 2000), 0, 1, -400, 400); // Generate z position based on Perlin noise
 
-        push();
-        translate(i * 50, j * 50, k * 50);
-        stroke(r, g, b);
-        ambientMaterial(r, g, b);
-        sphere(sphereSize);
-        pop();
-      }
-    }
+    let r = map(sin(frameCount * 0.01 + x + y + z), -1, 1, 0, 255);
+    let g = map(sin(frameCount * 0.02 + x + y + z), -1, 1, 0, 255);
+    let b = map(sin(frameCount * 0.03 + x + y + z), -1, 1, 0, 255);
+
+    push();
+    translate(x, y, z);
+    stroke(r, g, b);
+    ambientMaterial(r, g, b);
+    sphere(sphereSize);
+    pop();
   }
 
   angleX += 0.01;
   angleY += 0.02;
   angleZ += 0.03;
-}
-
-function mouseClicked() {
-  sphereSize += 1;
 }
