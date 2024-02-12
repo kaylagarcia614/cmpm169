a36277d91be4c52ca7629d90450cd1c4ca2fb30b
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-02-12 11:04:20 -0800

    test

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,52 +1,17 @@
-let dots = [];
-const layers = 10;
-let mic;
-let fft;
+let sound;
 
-
-function setup(){
-    createCanvas(windowWidth,windowHeight,WEBGL);
-		fft = new p5.FFT();
-		userStartAudio();
-		mic = new p5.AudioIn();
-		mic.start();
-		fft.setInput(mic);
+function preload() {
+    // Load the "allmyfellas.mp3" file
+    sound = loadSound('allmyfellas.wav');
 }
 
-function draw(){
-    background(255);
-    stroke(0);
-		strokeWeight(4)
-    noFill();
-    rotateX(0.25*PI);
-		rotateZ(frameCount/80);
-    let d = 30;
-    let x,y,z;
-		let spectrum = fft.analyze();
-		spectrum.shift();
-		spectrum.shift();
-		spectrum.shift();
-    smooth();
-		let i = 0;
-    for (let k = 0; k < layers; k++){
-      beginShape();
-      for (let a = 0; a < 2*PI; a+= 1/k * 1){
-					x = d*k*cos(a);
-          y = d*k*sin(a);
-          z = k*cos(frameCount/1);
-					//z *= noise(x/30,y/30);
-					if (i < spectrum.length){
-							z += spectrum[i++]*2;
-				smooth();
-		    stroke(255, 204, random(0,255));
-					}
-          vertex(x,y,z);
-      }
-      endShape(CLOSE);
-  	}
+function setup() {
+    createCanvas(400, 200);
+    // Play the loaded sound
+    sound.play();
 }
 
-function mousePressed(){
-
+function draw() {
+    background(220);
+    // Your drawing code here
 }
-
