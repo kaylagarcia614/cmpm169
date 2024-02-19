e01eea5b1af357f6f04c4e27ad9e87b37e127e9c
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-02-19 15:08:06 -0800

    added experiment  6

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,94 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+const msg = "WHOA"; // Text to display
+let msgSize = 200; // Adjust font size to make text more visible
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+const noiseParams = {
+    cumulative: true, // Whether noise displacements of points accumulate
+    noiseZoom: 0.006, // Perlin noise zoom
+    noiseOctaves: 4, // Perlin noise octaves
+    noiseFalloff: 0.5, // Perlin noise falloff
+    lineSpeed: 0.5, // Maximum amount each point can move each frame
+    direction: 1 // Direction of noise displacement: 1 for positive, -1 for negative
+};
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let font;
+let points = []; // Array to store points
+let bounds;
 
-// Globals
-let myInstance;
-let canvasContainer;
+function preload() {
+    font = loadFont('Anton.ttf');
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    let canvas=createCanvas(600, 600);
+    canvas.parent('canvas-container');
+    strokeWeight(6);
+    noiseDetail(noiseParams.noiseOctaves, noiseParams.noiseFalloff);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    bounds = font.textBounds(msg, 0, 0, msgSize);
+
+    for (let p of font.textToPoints(msg, 0, 0, msgSize, {
+        simplifyThreshold: 0
+    })) {
+        p.zOffset = random();
+        p.color = color(random(255), random(255), random(255)); // Generate random color for each point
+        points.push(p);
+    }
+
+    background(130);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+   
+    translate(width / 2, height / 2); // Translate to the center of the canvas
+    rotate(frameCount * 0.01); // Rotate the canvas on the z-axis (plane)
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    translate(-bounds.w / 2, bounds.h / 2); // Translate to adjust for text bounds
+
+    // Add a new dot randomly every second
+    if (frameCount % 60 == 0) { // Every second
+        let newPoint = font.textToPoints(msg, random(width), random(height), msgSize, {
+            simplifyThreshold: 0
+        })[0];
+        newPoint.zOffset = random();
+        newPoint.color = color(random(255), random(255), random(255)); // Generate random color for each point
+        points.push(newPoint);
+    }
+
+    for (let i = 0; i < points.length; i++) {
+        let p = points[i];
+
+        let noiseX = p.x * noiseParams.noiseZoom;
+        let noiseY = p.y * noiseParams.noiseZoom;
+        let noiseZ = frameCount * 0.01 + p.zOffset;
+
+        let noiseDisplacementX = map(noise(noiseX, noiseY, noiseZ), 0, 1, -noiseParams.lineSpeed, noiseParams.lineSpeed) * noiseParams.direction;
+        let noiseDisplacementY = map(noise(noiseX, noiseY, noiseZ + 214), 0, 1, -noiseParams.lineSpeed, noiseParams.lineSpeed) * noiseParams.direction;
+
+        if (noiseParams.cumulative) {
+            p.x += noiseDisplacementX;
+            p.y += noiseDisplacementY;
+        } else {
+            p.x += noiseDisplacementX - p.prevNoiseDisplacementX;
+            p.y += noiseDisplacementY - p.prevNoiseDisplacementY;
+        }
+
+        p.prevNoiseDisplacementX = noiseDisplacementX;
+        p.prevNoiseDisplacementY = noiseDisplacementY;
+
+        stroke(0); // Set stroke color to black
+        fill(p.color); // Use the random color for filling
+        ellipse(p.x, p.y, 8); // Draw each point with an ellipse
+
+        if (p.alpha == 1) { // Character break?
+            endShape(CLOSE);
+            beginShape();
+        }
+    }
+    endShape();
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function mouseClicked() {
+    // Toggle the direction of noise displacement
+    noiseParams.direction *= -1;
 }
